3b308a4dedec38e310d7e92139c59ee927a131b7
Azamat Kopzhasharov <kopzha01@gmail.com>
2023-12-13 23:52:24 +0600

    final step: added views

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import {PORT, mongoDBURL} from './config.js';
 import mongoose from 'mongoose';
 import {Book} from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
+
+// middleware for handling CORS policy
+
+// first option
+app.use(cors());
+// second option
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
 
 app.get('/', (request, response) => {
     console.log(request);
