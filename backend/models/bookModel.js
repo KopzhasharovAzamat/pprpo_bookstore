1e3a5aff43b07809d39e255113c7e7eed7a4b3c1
Azamat Kopzhasharov <kopzha01@gmail.com>
2023-12-15 18:09:45 +0600

    small change

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -5,7 +5,6 @@ const bookSchema = mongoose.Schema(
         title: {
             type: String,
             required: true,
-            unique: true,
         },
         author: {
             type: String,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema(
         email: {
             type: String,
             required: true,
+            unique: true,
         },
         passwordHash: {
             type: String,
