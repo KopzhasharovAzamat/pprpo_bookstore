7479be840bddbc7fd365c1ea9da35dab581ac00b
Azamat Kopzhasharov <kopzha01@gmail.com>
2023-12-26 16:31:56 +0600

    add authentication process

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -2,7 +2,12 @@ import express from 'express';
 import { Book } from '../models/bookModel.js';
 import {createBook, updateBook, deleteBook, getAllBooks, getBookById} from '../controllers/bookController.js';
 
+import { requireAuth } from '../middleware/requireAuth.js';
+
 const router = express.Router();
+
+// require auth for all book routes
+router.use(requireAuth)
 
 // POST a book
 router.post('/', createBook);
